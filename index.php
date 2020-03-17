34fc5152c257d95bc686f58e3f88fe376ce40d8f
Unknown <99053846@mydavinci.nl>
2020-03-17 14:35:58 +0100

    Added a pokemon and changed one

--- index.php
+++ index.php
@@ -4,11 +4,23 @@ require 'Pokemon.php';
 
 // ----------
 
-$pikachu =  new pokemon('Pikachu',
-                        'electric',
-                        '60',
-                        [(object)array('move'=>'Electric Ring', 'damage'=> 50), (object)array('move'=>'Pika Punch', 'damage'=> 20) ],
-                        (object)array('type'=>'Fire', 'mulitplier' => 1.5), (object)array('type' => 'Fighting', 'mulitplier' => 20));
+$pikachu =  new pokemon(
+  'Pikachu',
+  'lightning',
+  '60',
+  [(object)array('move'=>'Electric Ring', 'damage'=> 50), (object)array('move'=>'Pika Punch', 'damage'=> 20)],
+  (object)array('type'=>'Fire', 'mulitplier' => 1.5),
+  (object)array('type' => 'Fighting', 'mulitplier' => 20)
+);
+
+$charmeleon = new pokemon(
+  'charmeleon',
+  'fire',
+  '60',
+  [(object)array('move'=>'Head Butt', 'damage'=> 10), (object)array('move'=>'Flare', 'damage'=> 30)],
+  (object)array('type'=>'Water', 'mulitplier' => 2),
+  (object)array('type'=>'lightning', 'mulitplier' => 10)
+);
 
 echo $pikachu->__toString() . '<br>';
 
